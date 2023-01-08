4eb48a968a17f84e45021180381500b6a3745848
JI HYEON LEE <art_de4@naver.com>
2023-01-08 17:47:29 +0900

    join

--- Toy/src/main/java/login/service/impl/MemberServiceImpl.java
+++ Toy/src/main/java/login/service/impl/MemberServiceImpl.java
@@ -6,14 +6,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import login.dao.face.MemberDao;
+import login.dto.Member;
 import login.service.face.MemberService;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-	
+
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired MemberDao memberDao;
+
+	//로그인
+	@Override
+	public boolean login(Member member) {
+		int loginChk = memberDao.selectCntMember(member);
+		
+		logger.info("loginChk : {}", loginChk);
+
+		if( loginChk > 0 )	return true;
+		
+		return false;
+	}
+
+	@Override
+	public int idchk(String memberId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean join(Member member) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
