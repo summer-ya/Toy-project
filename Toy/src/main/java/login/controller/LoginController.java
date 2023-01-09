815f502e474ac7343ce4084e33a478acc0379b22
JI HYEON LEE <art_de4@naver.com>
2023-01-09 18:41:48 +0900

    login

--- Toy/src/main/java/login/controller/LoginController.java
+++ Toy/src/main/java/login/controller/LoginController.java
@@ -1,9 +1,12 @@
 package login.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import login.service.face.MemberService;
 
@@ -15,5 +18,14 @@ public class LoginController {
 
 	//서비스 객체
 	@Autowired private MemberService memberService;
+
+	//로그인
+	@GetMapping("/login")
+	public String login(HttpServletRequest req) {
+		req.setAttribute("message", "");
+		logger.info("/login [GET]성공");
+		return "login/login";
+
+	}
 
 }
