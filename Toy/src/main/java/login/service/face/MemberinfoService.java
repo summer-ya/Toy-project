24580fa8b71d980710c38223c6c1e32d076beb2f
JI HYEON LEE <art_de4@naver.com>
2023-01-28 20:12:38 +0900

    commit

--- Toy/src/main/java/login/service/face/MemberinfoService.java
+++ Toy/src/main/java/login/service/face/MemberinfoService.java
@@ -1,33 +1,33 @@
 package login.service.face;
 
-import login.dto.Member;
+import login.dto.Memberinfo;
 
-public interface MemberService {
+public interface MemberinfoService {
 	
 	/**
 	 * 로그인 인증 처리
 	 * 	
-	 * @param member - 입력한 ID/PW 정보
+	 * @param Memberinfo - 입력한 ID/PW 정보
 	 * @return 로그인 인증 경과
 	 */
-	public boolean login(Member member);
+	public boolean login(Memberinfo Memberinfo);
 
 	/**
 	 * 아이디 중복 체크
 	 * 
-	 * @param memberId
+	 * @param MemberinfoId
 	 * @return
 	 * @throws Exception
 	 */
-	public int idchk(String memberId);
+	public int idchk(String MemberinfoId);
 
 	/**
 	 * 신규 회원 가입
 	 * 
-	 * @param member - 신규 회원의 정보
+	 * @param Memberinfo - 신규 회원의 정보
 	 * @return 회원가입 결과
 	 */
-	public boolean join(Member member);
+	public boolean join(Memberinfo Memberinfo);
 
 
 }
